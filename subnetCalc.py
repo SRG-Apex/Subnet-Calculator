7ac5679feeb58baba6a30edee7ce8443a701d818
lenadraconai <ajbobay@mailfence.com>
2024-10-16 08:01:44 -0400

    Bug Fixes for Class B
    Fixed class B to calculate properly
    
    Class now calculates based on provided IP

--- subnetCalc.py
+++ subnetCalc.py
@@ -1,16 +1,18 @@
 import math
 from os import system,name
-CLASS = input("Enter Class: ").upper()
+
 IP = input("Enter IP: ")
 
 IP_Array = IP.split(".")
 if 224 > int(IP_Array[0]) >= 192:
     CLASS = "C"
+elif 127 < int(IP_Array[0]):
+    CLASS = "B"
+else: CLASS="A"
 
-IP = IP_Array[0] + "." + IP_Array[1] + "." + IP_Array[2] + "."
 
-S = input("Enter Subnets (Leave Blank if N/A): ")
-H = input("Enter Number of Usable Hosts (Leave Blank if N/A): ")
+S = int(input("Enter Subnets (Enter 0 if N/A): "))
+H = int(input("Enter Number of Usable Hosts (Enter 0 if N/A): "))
 
 ClassTable = {
     "C" : 3,
@@ -18,25 +20,30 @@ ClassTable = {
     "A" : 1
 }
 def nCalc(CLASS, S, H):
-    if S:
+    print(f"{S}\n{H}")
+    if not S==0:
         n = math.ceil(math.log2(S)) + 8*ClassTable[CLASS]
     elif H:
-        n = 32-(math.ceil(math.log2(H))) + 8*ClassTable[CLASS]
+        n = 32-(math.ceil(math.log2(H)))
     return n 
 
 n = nCalc(CLASS, S, H)
-H = 2**(32 - n - 8*ClassTable[CLASS])
+H = 2**(32 - n)
 S = 2**(n - 8*ClassTable[CLASS])
 
-activeOctet = math.ceil(n/4)
+activeOctet = math.ceil(n/8)
 
 if CLASS == "B":
     subnet3 = 255
+    print(activeOctet)
     if activeOctet == 4:
         subnet2 = 255
         subnet1 = 0
-        for i in range(n):
+        print(n)
+        for i in range(n-24):
+            print(i)
             subnet1 += (2**(7-i))
+            print(subnet1)
     else: 
         subnet2 = 0
         for i in range(n-16):
@@ -46,17 +53,18 @@ if CLASS == "C":
     subnet3 = 255
     subnet2 = 255
     subnet1 = 0
-    for i in range(n):
+    for i in range(n-24):
         subnet1 += (2**(7-i))
 
 subnetmask = f"255.{subnet3}.{subnet2}.{subnet1}"
-system('cls' if name == 'nt' else 'clear')
+# system('cls' if name == 'nt' else 'clear')
 print("--- Network Statistics: ---\n")
-
+print(f"Class: {CLASS}")
 print(f"Number of Subnets (S): {S}")
 print(f"Total Host (H) {H}")
 print(f"Number of Usable Hosts: {H-2}")
-print(f"Bits Borrowed (N): {n}")
+print(f"Internal N: {n}")
+print(f"Bits Borrowed (N): {n-(8*ClassTable[CLASS])}")
 print(f"Subnet Mask: {subnetmask}")
 
 if input("See Ranges? (y/N):") == "y":
